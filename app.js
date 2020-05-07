233e3189be95d5b8bd939c1d26861300ebb9bfc4
Zach Ayers <zayers.dev@gmail.com>
2020-05-07 10:50:31 -0500

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+// IMPORT MODULES
+const path = require('path');
+const express = require('express'); // express framework
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
+
+// TODO Import Routes
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const authRouter = require('./routes/authRoutes');
+
+// INIT EXPRESS FRAMEWORK
+const app = express();
+app.enable('trust proxy');
+
+// Implement CORS
+app.use(cors()); // Access-Control-Allow-Origin *
+app.options('*', cors());
+
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
+// USE HELMET PACKAGE TO SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+// *RATE LIMITER
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.',
+});
+
+// ADD RATE LIMITER TO THE API ROUTE
+app.use('/api', limiter);
+
+// BODY PARSER - READ DATA FROM BODY INTO REQ.BODY
+app.use(express.json({ limit: '10kb' })); // Limit body to 10kb
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); // Form Parser
+app.use(cookieParser());
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS ATTACKS
+app.use(xssClean());
+
+//* COMPRESSION FOR TEXT SENT TO CLIENTS
+app.use(compression());
+
+// INIT ROUTERS
+app.use('/api/v1/auth', authRouter);
+
+// ROUTE HANDLER FOR NON-EXISTENT ROUTES
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// ADD GLOBAL ERROR HANDLER MIDDLEWARE
+app.use(globalErrorHandler);
+
+// EXPORT THIS MODULE
+module.exports = app;
---
+++ routes/authRoutes.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+// IMPORT MODULES
+const express = require('express'); // express framework
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+// MOUNT ROUTER
+const router = express.Router();
+
+// *SIGN UP/LOGIN ROUTES
+router.post('/signup', authController.signup);
+router.post('/signin', authController.login);
+router.get('/signout', authController.logout);
+
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUser,
+);
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser);
+
+// EXPORT ROUTER
+module.exports = router;
